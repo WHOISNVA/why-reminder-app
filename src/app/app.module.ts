8406738487c5f833e7e4bbc6d2e5bff3bcb69375
NVA <dapperartisancompany@gmail.com>
2023-10-10 11:37:23 -0500

    Fix form UI and add safe pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule  } from "@angular/forms";
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { SafePipe } from './safe.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    WhiteboardComponent
+    WhiteboardComponent,
+    SafePipe
     
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
