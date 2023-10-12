5a793466fdabf6a0637476959d204e937390496d
NVA <dapperartisancompany@gmail.com>
2023-10-11 22:45:22 -0500

    Added Title Banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { AppComponent } from './app.component';
 import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { SafePipe } from './safe.pipe';
+
 
 
 @NgModule({
@@ -23,10 +20,6 @@ import { SafePipe } from './safe.pipe';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
     
   ],
   providers: [],
